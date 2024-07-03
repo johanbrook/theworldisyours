a2513cd8893034e2f23d788f148c3cf5a1642d42
Johan Brook <johan@lookback.io>
2024-07-03 15:10:06 +0200

    /archive -> /all

--- all.page.ts
+++ all.page.ts
@@ -3,7 +3,7 @@ export const layout = "layouts/archive.vto";
 export default function* ({ search, paginate }: Lume.Data) {
     const posts = search.pages("type=post", "date=desc");
     const options = {
-        url: (n: number) => (n == 1 ? "/archive/" : `/archive/${n}/`),
+        url: (n: number) => (n == 1 ? "/all/" : `/all/${n}/`),
         size: 100,
     };
 
